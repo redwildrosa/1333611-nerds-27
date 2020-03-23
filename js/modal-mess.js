08ccb4fecbd556606ea21aabef4abfc80cb1bd58
redwildrosa <redwildrosa@yandex.ru>
2020-03-23 10:45:25 +0300

    Защита-2

--- js/modal-mess.js
+++ js/modal-mess.js
@@ -1,26 +1,28 @@
 var modalMess = document.querySelector(".modal-mess");
 var mapButton = document.querySelector(".main-map .button");
-var modalButtonCross = document.querySelector(".modal-mess .button-cross");
+var modalButtonCross = modalMess.querySelector(".button-cross");
 
 var modalForm = modalMess.querySelector("form");
 var fieldName = modalMess.querySelector("[name=fullname]");
 var fieldEmail = modalMess.querySelector("[name=email]");
+var fieldComment = modalMess.querySelector("[name=comment]");
 var modalButton = modalMess.querySelector("[type=submit]");
 
 var isStorageSupport = true;
 var storageName = "";
 var storageEmail = "";
+var storageComment = "";
 
 try {
   storageName = localStorage.getItem("fieldName");
   storageEmail = localStorage.getItem("fieldEmail");
+  storageComment = localStorage.getItem("fieldComment");
 } catch (err) {
   isStorageSupport = false;
 }
 
 mapButton.addEventListener("click", function (evt) {
   evt.preventDefault();
-  console.log("Клик-открыть");
   modalMess.classList.add("modal-mess--open");
 
   if (storageName) {
@@ -29,32 +31,36 @@ mapButton.addEventListener("click", function (evt) {
   } else {
     fieldName.focus();
   }
+
   if (storageEmail) {
     fieldEmail.value = storageEmail;
-    modalButton.focus();
+    if (storageName) {
+      fieldComment.focus();
+    }
+  }
+
+  if (storageComment) {
+    fieldComment.value = storageComment;
   }
 });
 
 modalButtonCross.addEventListener("click", function (evt) {
   evt.preventDefault();
-  console.log("Клик-закрыть");
   modalMess.classList.remove("modal-mess--open");
-  popup.classList.remove("modal-mess--error");
+  modalMess.classList.remove("modal-mess--error");
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27 && modalMess.classList.contains("modal-mess--open")) {
     evt.preventDefault();
-    console.log("ESC-закрыть");
     modalMess.classList.remove("modal-mess--open");
     modalMess.classList.remove("modal-mess--error");
   }
 });
 
 modalForm.addEventListener("submit", function (evt) {
-  if (!fieldName.value || !fieldEmail.value) {
+  if (!fieldName.value || !fieldEmail.value || !fieldComment.value) {
     evt.preventDefault();
-    console.log("Заполните обязательные поля");
     modalMess.classList.remove("modal-mess--error");
     modalMess.offsetWidth = modalMess.offsetWidth;
     modalMess.classList.add("modal-mess--error");
@@ -62,6 +68,7 @@ modalForm.addEventListener("submit", function (evt) {
     if (isStorageSupport) {
       localStorage.setItem("fieldName", fieldName.value);
       localStorage.setItem("fieldEmail", fieldEmail.value);
+      localStorage.setItem("fieldComment", fieldComment.value);
     }
   }
 });
